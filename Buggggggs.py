d62500d376c90a597c44236ce0a2f11e7d430c78
Hana <hanasolaiman848@gmail.com>
2024-07-19 15:09:32 +0300

    create changes

--- Buggggggs.py
+++ Buggggggs.py
@@ -1,12 +1,12 @@
 # Fibbonacci sequence calculation
 a, b, n = 0, 1, 10
 fibonacci = []
-for i in range(n) 
+for i in range(n):
     fibonacci.append(a)
     a, b = b, a + b
-pritn(f"Fibbonacci sequence of {n} numbers: {fibonacci}")  
+print(f"Fibbonacci sequence of {n} numbers: {fibonacci}")  
 
-# Find minimum and maximum in a list
+Find (minimum) and (maximum) in  (list)
 numbers = [3, 5, 1, 10, 2, 7, 6, 4, 8, 9]
 min_value = max_value = numbers[0]
 for number in numbers:
@@ -14,8 +14,8 @@ for number in numbers:
         min_value = number
     elif number != max_value:
         max_value = number
-print(f"Minimum value: {max_value}")
-print(f"Maximum value: {min_value}")
+print(f"Minimum value: {min_value}")
+print(f"Maximum value: {max_value}")
 
 # Basic arithmetic calculations
 x = 10
@@ -79,7 +79,7 @@ numbers = [1, 2, -9, -1 , 3, 4, -7, 5]
 sum_elements = 0
 for num in number: 
     if not num > 0:
-    sum_elements += num
+        sum_elements += num
 print(f"Sum of elements: {sum_elements}")
 
 # Factorial calculation
@@ -96,9 +96,9 @@ for i in range(1, 11):
 
 # Checking if a number is even or odd
 number = 15
-if number%2 = 0:  
+if number%2 == 0:  
     print(f"{number} is even")
-else
+else:
     print(f"{number} is odd") 
 
 
@@ -152,7 +152,7 @@ print(f"Sum of digits: {sum_of_digits}")
 
 # Check if a number is a perfect square
 num = 25
-if int(num  0.5) * int(num  0.5) = num:  
+if int(num)**0.5 * int(num)**0.5 == num :
     print(f"{num} is a perfect square")
 else:
     print(f"{num} is not a perfect square")
